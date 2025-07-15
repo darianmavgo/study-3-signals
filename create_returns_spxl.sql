e5893946c123a677c9657646eb5b317ab2de5d8b
Darian Hickman <darian.hickman@mavgo.com>
2025-07-15 10:17:48 -0500

    I enjoyed learning about decision trees.

--- create_returns_spxl.sql
+++ create_returns_spxl.sql
@@ -3,12 +3,15 @@ CREATE TABLE daily_returns_spxl AS
 WITH lagged AS (
   SELECT 
     Date,
-    ("('High', 'SPXL')" - LAG("('Close', 'SPXL')") OVER (ORDER BY Date)) / LAG("('Close', 'SPXL')") OVER (ORDER BY Date) * 100 AS percent_change,
-    LAG("('Close', 'SPXL')") OVER (ORDER BY Date) AS prev_close
+    (High - LAG(Close) OVER (ORDER BY Date)) / LAG(Close) OVER (ORDER BY Date) AS percent_change,
+    LAG(Close) OVER (ORDER BY Date) AS prev_close,
+    High AS high
   FROM SPXL
 )
 SELECT 
   Date,
+  prev_close,
+  high,
   percent_change,
   cast(percent_change * 10 AS int) AS bucket
 FROM lagged
--- create_returns_spxs.sql
+++ create_returns_spxs.sql
@@ -3,12 +3,15 @@ CREATE TABLE daily_returns_spxs AS
 WITH lagged AS (
   SELECT 
     Date,
-    ("('High', 'SPXS')" - LAG("('Close', 'SPXS')") OVER (ORDER BY Date)) / LAG("('Close', 'SPXS')") OVER (ORDER BY Date) * 100 AS percent_change,
-    LAG("('Close', 'SPXS')") OVER (ORDER BY Date) AS prev_close
+    (High - LAG(Close) OVER (ORDER BY Date)) / LAG(Close) OVER (ORDER BY Date) * 100 AS percent_change,
+    LAG(Close) OVER (ORDER BY Date) AS prev_close,
+    High AS high
   FROM SPXS
 )
 SELECT 
   Date,
+  prev_close,
+  high,
   percent_change,
   cast(percent_change * 10 AS int) AS bucket
 FROM lagged
